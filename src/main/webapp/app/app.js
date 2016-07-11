935198d9f97d3cf5f0e2a58e1bb6fc2358154a94
Raihan <raihan.dk@gmail.com>
2016-07-11 19:20:53 +0600

    Added angular-ui-router

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -3,7 +3,9 @@ var app = angular.module('app', ['ui.router', 'restangular']);
 app.config(['$stateProvider', 'RestangularProvider', function ($stateProvider, RestangularProvider) {
     // ui-router config
     $stateProvider
-        .state('index', {url: "", views: {"body": {template: "Home Page"}}})
+        .state('index', {
+            url: "",
+            views: {"body": {template: "Home Page"}}})
         .state('user', {
             url: "/user",
             views: {"body": {templateUrl: "app/user/user.tpl.html", controller: "userCtrl"}}
